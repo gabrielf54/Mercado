db2dc6f5865b664e9bc50db80d382df0db09af9c
gabrielf54 <gabriel.nuno22@gmail.com>
2023-05-22 11:59:14 -0300

    commit initial

---
+++ Mercado/src/view/Teste.java
@@ -0,0 +1,16 @@
+package view;
+
+import model.Produto;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Produto ps4 = new Produto("Playstation 4", 1734.7);
+		Produto xbox = new Produto("Xbox One", 2734.7);
+		
+		System.out.println(ps4);
+		System.out.println(xbox);
+
+	}
+
+}
